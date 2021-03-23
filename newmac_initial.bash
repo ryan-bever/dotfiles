fc40a30cd3a73bb77f5f8dfbbb69888964181c93
Ryan Bever <ryan.bever@netapp.com>
2021-03-22 18:11:56 -0600

    3/22/2021

--- newmac_initial.bash
+++ newmac_initial.bash
@@ -7,7 +7,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 # Install Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
 # Pin the version of git that doesn't clobber autocomplete
@@ -19,7 +19,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Set brew to not update on every call during this script
 HOMEBREW_NO_AUTO_UPDATE=1
 
-
+brew install git
 brew cask install iterm2 
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
@@ -42,15 +42,8 @@ brew cask install macvim
 
 
 
-
 brew install pyenv
 brew install pyenv-virtualenv
-
-# This allows pyenv to guess a version when e.g. #!/usr/bin/env python3.7 is in a shebang and multiple 3.7 versions are installed
-git clone git://github.com/concordusapps/pyenv-implict.git ~/.pyenv/plugins/pyenv-implict
-# brew install zlib
-# pyenv install 3.8.2
-# pyenv global 3.8.2
 
 
 brew cask install scroll-reverser
@@ -58,45 +51,10 @@ brew install the_silver_searcher
 brew cask install diffmerge
 
 
-# brew install jenv
-# brew cask install java
-# brew tap caskroom/versions
-# brew cask install java8
-######
-# jenv add /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home
-# jenv add /Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home
-######
-# jenv global 1.8
-# jenv enable-plugin scala
-# jenv enable-plugin export
-# jenv rehash
-
 # utils
 brew install watch
 brew install fswatch
 brew install telnet
 brew install jq
 brew install sshrc
-
-# brew install scala 
-# brew install sbt
-
-# brew install autossh
-# brew install mysql-client
-
-brew cask install pycharm
-# brew cask install intellij-idea
-
-# brew cask install hipchat
-brew cask install docker
-brew cask install macdown
-brew cask install postman
-
-# Minikube stuff
-brew cask install virtualbox
-brew cask install minikube
-brew install kubectl
-
-# i2cssh
-# gem install i2cssh --user-install
 
